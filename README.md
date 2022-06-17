27fd10cba46c7016972f4c9e7b9df416bbcb80df
Aryan Shukla <2020ucs0077@iitjammu.ac.in>
2022-06-17 16:31:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Weather App
+
+Just type the name of a city and the weather coditions like temperature will be displayed on the screen.
+
+
+
+## Authors
+
+- [Aryan Shukla](https://www.github.com/aryannewyork)
+
+
+## Deployment
+
+To deploy this project run in the terminal the following command after cd into the project directory.
+
+```bash
+  node app.js
+```
+
+- It should look something like this.
+![Preview](https://user-images.githubusercontent.com/79625246/174285308-d5989416-57b9-4467-b429-e0c498be440e.jpg)
+
+- After you click on "Go" button, you will see something like this. Containing the weather conditions.
+
+
+
+![Preview 2](https://user-images.githubusercontent.com/79625246/174285300-1de774fb-e39f-4a82-ba08-1fcaaf3c284c.jpg)
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+    
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const APIKey = "c5cfd327d3c911e51bd66d0d53c50977";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + APIKey + "&units=" + units;
+    https.get(url, function(response){
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const iconURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p> The weather in " + query + " is " + weatherDescription + "</p>");
+            res.write("<h1> The temperature in " + query + " is " + temp + " degree Celsius. </h1>");
+            res.write("<img src = " + iconURL + ">");
+            res.send();
+        });
+    });
+});
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("The server is running at port 3000...");
+});
